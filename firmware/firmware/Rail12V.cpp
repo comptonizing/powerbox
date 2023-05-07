02bd5441966f63f0b1681dc5982f9fc19c9665a9
Philipp Weber <philipp.ph.weber@fau.de>
2023-05-07 23:50:49 +0200

    Add 12V Rail control

---
+++ firmware/firmware/Rail12V.cpp
@@ -0,0 +1,19 @@
+#include "Rail12V.h"
+
+Rail12V::Rail12V(uint8_t pin) : m_pin(pin) {
+  off();
+}
+
+void Rail12V::on() {
+  digitalWrite(m_pin, HIGH);
+  m_state = HIGH;
+}
+
+void Rail12V::off() {
+  digitalWrite(m_pin, LOW);
+  m_state = LOW;
+}
+
+uint8_t Rail12V::state() {
+  return m_state;
+}
