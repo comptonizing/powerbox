7c3abeaaa031d4262a1c0afeb6c6625120e0b693
Philipp Weber <philipp.ph.weber@fau.de>
2023-05-07 17:02:21 +0200

    Add pin enumeration

--- firmware/firmware/firmware.ino
+++ firmware/firmware/firmware.ino
@@ -1,3 +1,20 @@
+// Temperature probe 1
+#define TEMP1DATA 2
+// Temperature probe 2
+#define TEMP2DATA 3
+
+// 4x12V Output
+#define RAIL12V 6
+
+// Buck converter chip enable
+#define ADJ 8
+
+// Dew heater 1 PWM
+#define DH1PWM 9
+// Dew heater 2 PWM
+#define DH2PWM 10
+
+
 void setup(void)
 {
   Serial.begin(9600);
