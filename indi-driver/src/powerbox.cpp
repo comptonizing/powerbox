ff84b51cd7d232ae46357cf60bb8f7524eb1c689
Philipp Weber <philipp.ph.weber@fau.de>
2023-06-21 21:45:51 +0200

    Add basic sensor readings

--- indi-driver/src/powerbox.cpp
+++ indi-driver/src/powerbox.cpp
@@ -1,4 +1,7 @@
 #include "powerbox.h"
+#include <defaultdevice.h>
+
+using json = nlohmann::json;
 
 static std::unique_ptr<Powerbox> shelyakDriver(new Powerbox());
 
@@ -54,11 +57,24 @@ bool Powerbox::sendCommand(const char *cmd, char *rsp) {
     return false;
   }
 
-  if ( (rc = tty_read_section(PortFD, rspBuff, '$', 5, &nbytes_read)) != TTY_OK ) {
+  LOG_DEBUG("RCV");
+
+  // Somtimes there's garbage on the line so read until the next #
+  rspBuff[0] = '\0';
+  while ( rspBuff[0] != '#' ) {
+    if ( (rc = tty_read(PortFD, rspBuff, 1, TIMEOUT, &nbytes_read) ) != TTY_OK ) {
+      tty_error_msg(rc, err, ERRBUFF);
+      LOGF_ERROR("Error reading response: %s", err);
+      return false;
+    }
+  }
+
+  if ( (rc = tty_read_section(PortFD, rspBuff+1, '$', TIMEOUT, &nbytes_read)) != TTY_OK ) {
     tty_error_msg(rc, err, ERRBUFF);
     LOGF_ERROR("Error reading response: %s", err);
     return false;
   }
+  rspBuff[nbytes_read+1] = '\0';
 
   LOGF_DEBUG("RSP: %s", rspBuff);
   memcpy(rsp, rspBuff+1, strlen(rspBuff)-2);
@@ -67,8 +83,7 @@ bool Powerbox::sendCommand(const char *cmd, char *rsp) {
     LOG_ERROR("Checksum error");
     return false;
   }
-  rsp[strlen(rspBuff)-5] = '\0';
-
+  rsp[strlen(rsp)-2] = '\0';
   return true;
 }
 
@@ -79,11 +94,22 @@ bool Powerbox::Handshake() {
   if ( ! sendCommand(cmd, rsp) ) {
     return false;
   }
+  json data;
+  try {
+    data = json::parse(rsp);
+  } catch (...) {
+    LOG_ERROR("JSON parse error");
+    return false;
+  }
+  if ( data.contains("Error") ) {
+    LOGF_ERROR("Device error: %s", data["Error"].template get<std::string>().c_str());
+    return false;
+  }
   return true;
 }
 
 bool Powerbox::initProperties() {
-  DefaultDevice::initProperties();
+  INDI::DefaultDevice::initProperties();
   setDriverInterface(AUX_INTERFACE);
   addDebugControl();
   addConfigurationControl();
@@ -95,6 +121,17 @@ bool Powerbox::initProperties() {
       return Handshake();
       });
   registerConnection(serialConnection);
+
+  IUFillNumber(&VoltageN[0], "VOLTAGE", "Voltage [V]", "%0.2f", 0.0, 99.0, 0.01, 0.0);
+  IUFillNumberVector(&VoltageNP, VoltageN, 1, getDeviceName(), "VOLTAGE", "Power",
+      MAIN_CONTROL_TAB, IP_RO, 0, IPS_IDLE);
+
+  IUFillNumber(&EnvN[TEMPERATURE], "TEMPERATURE", "Temperature [°C]", "%.1f", -100.0, 100.0, 0.1, 0.0);
+  IUFillNumber(&EnvN[HUMIDITY], "HUMIDITY", "Rel. Humidity [%]", "%.1f", 0.0, 100.0, 0.1, 50.0);
+  IUFillNumber(&EnvN[PRESSURE], "PRESSURE", "Pressure [mbar]", "%.1f", 0.0, 2000.0, 0.1, 1013.0);
+  IUFillNumber(&EnvN[DEWPOINT], "DEWPOINT", "Dewpoint [°C]", "%.1f", -100.0, 100.0, 0.1, 0.0);
+  IUFillNumberVector(&EnvNP, EnvN, 4, getDeviceName(), "ENVIRONMENT", "Environment",
+      MAIN_CONTROL_TAB, IP_RO, 0, IPS_IDLE);
   return true;
 }
 
@@ -125,7 +162,86 @@ uint16_t Powerbox::crcCalc(const char *str) {
 }
 
 bool Powerbox::loadConfig(bool silent, const char *property) {
+  // Everything ignored here, all parameters come from the device
   (void) silent;
   (void) property;
   return true;
+}
+
+bool Powerbox::updateProperties() {
+  INDI::DefaultDevice::updateProperties();
+  if ( isConnected() ) {
+    defineProperty(&VoltageNP);
+    defineProperty(&EnvNP);
+    update();
+  } else {
+    deleteProperty(VoltageNP.name);
+    deleteProperty(EnvNP.name);
+  }
+  return true;
+}
+
+void Powerbox::setVoltage(const json& data) {
+  VoltageNP.s = IPS_BUSY;
+  IDSetNumber(&VoltageNP, nullptr);
+  try {
+    VoltageN[0].value = data["V"].template get<double>();
+    VoltageNP.s = IPS_OK;
+  } catch (...) {
+    VoltageNP.s = IPS_ALERT;
+    IDSetNumber(&VoltageNP, nullptr);
+    throw;
+  }
+  IDSetNumber(&VoltageNP, nullptr);
+}
+
+void Powerbox::setEnvironment(const json& data) {
+  EnvNP.s = IPS_BUSY;
+  IDSetNumber(&EnvNP, nullptr);
+  try {
+    EnvN[TEMPERATURE].value = data["E"]["T"].template get<double>();
+    EnvN[HUMIDITY].value = data["E"]["H"].template get<double>();
+    EnvN[PRESSURE].value = data["E"]["P"].template get<double>();
+    EnvN[DEWPOINT].value = data["E"]["D"].template get<double>();
+    EnvNP.s = IPS_OK;
+  } catch (...) {
+    EnvNP.s = IPS_ALERT;
+    IDSetNumber(&EnvNP, nullptr);
+    throw;
+  }
+  IDSetNumber(&EnvNP, nullptr);
+}
+
+bool Powerbox::update() {
+  char cmd[] = "status";
+  char rsp[RSPBUFF];
+  if ( ! sendCommand(cmd, rsp) ) {
+    return false;
+  }
+  json data;
+  try {
+    data = json::parse(rsp);
+  } catch (...) {
+    LOGF_ERROR("Error parsing JSON: %s", rsp);
+    return false;
+  }
+  if ( data.contains("Error") ) {
+    LOGF_ERROR("Device error: %s", data["Error"].template get<std::string>().c_str());
+    return false;
+  }
+  try {
+    setVoltage(data);
+    setEnvironment(data);
+  } catch (...) {
+    LOG_ERROR("Could not decode values from device");
+    return false;
+  }
+  return true;
+}
+
+void Powerbox::TimerHit() {
+  if ( isConnected() ) {
+    update();
+  }
+  SetTimer(getCurrentPollingPeriod());
 }
--- indi-driver/src/powerbox.h
+++ indi-driver/src/powerbox.h
@@ -13,6 +13,9 @@
 #define ERRBUFF 256
 #define RSPBUFF 1024
 
+#define TIMEOUT 2
+
+using json = nlohmann::json;
 
 namespace Connection {
   class Serial;
@@ -26,12 +29,21 @@ class Powerbox : public INDI::DefaultDevice {
     virtual const char *getDefaultName() override;
     virtual bool initProperties() override;
     virtual bool loadConfig(bool silent, const char *property) override;
-    /*
-    virtual void TimerHit() override;
     virtual bool updateProperties() override;
-    */
+    virtual void TimerHit() override;
   private:
     Connection::Serial *serialConnection{nullptr};
+    INumber VoltageN[1];
+    INumberVectorProperty VoltageNP;
+    INumber EnvN[4];
+    INumberVectorProperty EnvNP;
+
+    enum {
+      TEMPERATURE,
+      HUMIDITY,
+      PRESSURE,
+      DEWPOINT
+    };
 
     bool sendCommand(const char *cmd, char *rsp);
     virtual bool Handshake();
@@ -40,4 +52,8 @@ class Powerbox : public INDI::DefaultDevice {
     uint16_t crcCalc(const char *str);
     void cmdCrc(const char *cmd, char *out);
     bool checkCrc(const char *rsp);
+    const json& getJSON();
+    bool update();
+    void setVoltage(const json& data);
+    void setEnvironment(const json& data);
 };
